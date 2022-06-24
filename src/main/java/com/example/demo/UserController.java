203f85ead2fca1017cb00bb8937b72a6d59bfcb6
rimbo <rimbo@nongshim.co.kr>
2022-06-24 15:19:01 +0900

    [FIX] ip hostname

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,32 @@ public class UserController {
 
     @GetMapping("/user/{name}")
     public String getPatientInfo(HttpServletRequest request, @PathVariable("name") String name) {
+        return name;
+    }
+
+    @GetMapping("/ip")
+    public String getIp() {
+
+        String ip = "Unknown";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        return ip;
+    }
+
+    @GetMapping("/host-name")
+    public String getHostName() {
+
+        String name = "Unknown";
+        try {
+            name = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
         return name;
     }
 }
