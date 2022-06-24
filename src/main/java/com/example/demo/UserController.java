63c3080216a000fcff65c7ab56780d2055b5cf28
rimbo <rimbo@nongshim.co.kr>
2022-06-24 13:18:10 +0900

    [FIX] healthcheck 추가

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,14 +4,17 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/{name}")
+    @GetMapping("/")
+    public String getHealth() {
+        return "healthCheck";
+    }
+
+    @GetMapping("/user/{name}")
     public String getPatientInfo(HttpServletRequest request, @PathVariable("name") String name) {
         return name;
     }
